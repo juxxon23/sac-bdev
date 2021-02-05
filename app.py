da4bb722d88fb3171d3582f592929b7a359c76c0
DarkMatter <juxxon23@hotmail.com>
2021-02-05 12:47:53 -0500

    ajustes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dwmdwuxrbsmznj:ced17943385fa0ef9f237f025f138b0200ebd897f32a01bc7d9908023bcebf7c@ec2-52-204-113-104.compute-1.amazonaws.com:5432/dc3cnsfmu7o2u'
 #app.config["MONGO_URI"] = "mongodb://localhost:27017/sac-ddb" 
-CORS(app, support_credentials=True)
+CORS(app, support_credentials=True, resources={"*": {"origins": "*"}})
 db.init_app(app)
 
 # User routes
@@ -16,3 +16,6 @@ app.add_url_rule(user['signin'], view_func=user['view_func_signin'])
 
 # Document routes
 app.add_url_rule(document['document'], view_func=document['view_func_document'])
+
+if __name__ == "__main__":
+    app.run()
